4007f0fa361a713aaf2494f6eef33a7d3566293d
Sofia394914 <a394914@uach.mx>
2026-09-15 09:24:24 -0600

    Update app_control_de_acceso.py

--- app_control_de_acceso.py
+++ app_control_de_acceso.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.title("Control de acceso")
+
+edad = st.number_input("Edad:", min_value=0, value=18)
+
+tiene_identificacion = st.checkbox("Tiene identificación")
+
+# Condición completada
+if edad >= 18 and tiene_identificacion:
+    st.write("Puede ingresar.")
+else:
+    st.write("No puede ingresar.")
